dbab4a1bd5174579152c97d0522c8417e3829e23
Abdulrahman-Haji <77126546+ENG-CJ@users.noreply.github.com>
2022-09-25 20:17:59 +0300

    initial

---
+++ Project/GYM MANAGEMENT SYSTEM/Dashboard.cs
@@ -0,0 +1,31 @@
+﻿using GYM_MANAGEMENT_SYSTEM.DataConnection;
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace GYM_MANAGEMENT_SYSTEM
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            Connections cn = new Connections();
+            SqlConnection conn = cn.GetSqlConnection();
+            conn.Open();
+            var command = new SqlCommand("select *from users", conn);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                label1.Text = reader.GetString(0);
+            }
+            reader.Close();
+            conn.Close();
+
+        }
+    }
+}
---
+++ Project/GYM MANAGEMENT SYSTEM/DataConnection/Connections.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace GYM_MANAGEMENT_SYSTEM.DataConnection
+{
+    /// <summary>
+    /// Connects To The Sql Server Databases 
+    /// </summary>
+    public class Connections
+    {
+        /// <summary>
+        /// returns Sql connection From Current Server 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+
+
+        public SqlConnection GetSqlConnection()
+        {
+            string databaseName = "gym_db.mdf";
+            string PathName = Path.GetFullPath(Environment.CurrentDirectory);
+
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + PathName + @"\" + databaseName + ";Integrated Security=true;";
+
+            var SqlConnection = new SqlConnection(connectionString);
+
+            return SqlConnection;
+        }
+    }
+}
